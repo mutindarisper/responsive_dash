b068e96b14c793310350d153acf7ed67d3767c2a
Risper <mutindarisper@gmail.com>
2024-02-02 18:34:44 +0300

    added bottom drawer

--- src/components/ParentComponent.tsx
+++ src/components/ParentComponent.tsx
@@ -1,12 +1,41 @@
 import React, { useState } from 'react'
 import ChildComponent from './ChildComponent'
+import { Button, Drawer, } from '@mui/material';
+// import { useTheme } from '@mui/styles';}
+import { makeStyles } from '@mui/styles';
 
-type ParentComponentProps =  {
+type ParentComponentProps = {
     parentData: string;
-  }
+}
+
+const drawerWidth = 240;
+//   const theme = useTheme()
+const useStyles = makeStyles({
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+        bottom: 0,
+    },
+    content: {
+        // flexGrow: 1,
+        width:'20em',
+        height:'15em'
+
+        //   padding: '10em',
+    },
+});
+
 
 const ParentComponent = () => {
     const [parentState, setParentState] = useState<string>('Hello from Parent!');
+    const classes = useStyles();
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+
+
     const monthlySalesData = [
         { month: 'January', sales: 1200 },
         { month: 'February', sales: 1500 },
@@ -20,42 +49,65 @@ const ParentComponent = () => {
         { month: 'October', sales: 2100 },
         { month: 'November', sales: 1800 },
         { month: 'December', sales: 2200 },
-      ];
+    ];
 
-      const [chartData, setChartData] = useState({
+    const [chartData, setChartData] = useState({
         labels: monthlySalesData.map((entry) => entry.month),
         datasets: [
-          {
-            label: 'Monthly Sales',
-            data: monthlySalesData.map((entry) => entry.sales),
-            backgroundColor: 'rgba(75,192,192,0.2)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderWidth: 1,
-          },
-        ],
-      });
-
-
-      const generateRandomSales = () => {
-        const updatedData = {
-          ...chartData,
-          datasets: [
             {
-              ...chartData.datasets[0],
-              data: monthlySalesData.map(() => Math.floor(Math.random() * 1000)),
+                label: 'Monthly Sales',
+                data: monthlySalesData.map((entry) => entry.sales),
+                backgroundColor: 'rgba(75,192,192,0.2)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderWidth: 1,
             },
-          ],
-        };
-    
-        setChartData(updatedData);
-      };
+        ],
+    });
 
-  return (
-    <div className="parent_data">
-        <button onClick={generateRandomSales}>Generate Random Sales</button>
-        <ChildComponent parentData={parentState} parentChartData={chartData} />
-    </div>
-  )
+
+    const generateRandomSales = () => {
+        const updatedData = {
+            ...chartData,
+            datasets: [
+                {
+                    ...chartData.datasets[0],
+                    data: monthlySalesData.map(() => Math.floor(Math.random() * 1000)),
+                },
+            ],
+        };
+
+        setChartData(updatedData);
+        toggleDrawer()
+    };
+
+    const toggleDrawer = () => {
+        setIsDrawerOpen(!isDrawerOpen);
+    };
+
+
+    return (
+        <div className="parent_data">
+            <button onClick={generateRandomSales}>Generate Random Sales</button>
+
+            <Drawer
+                anchor="bottom"
+                open={isDrawerOpen}
+                onClose={toggleDrawer}
+                className={classes.drawer}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                {/* Drawer content goes here */}
+                <div className={classes.content}>
+                    <ChildComponent parentData={parentState} parentChartData={chartData} />
+                </div>
+            </Drawer>
+
+
+
+        </div>
+    )
 }
 
 export default ParentComponent
